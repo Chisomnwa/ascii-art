b0bf66d296aac7c2f9cf3417add057d7cba9878a
cnnamani <nnamanichisomnc@gmail.com>
2026-03-09 12:07:08 +0100

    Create the full program that builds graphic representation using ASCII

--- ascii/render.go
+++ ascii/render.go
@@ -6,7 +6,22 @@ import (
 	"strings"
 )
 
+// func Render(text string, banner string) {
+// 	bannerLines := readBanner(banner)
+
+// 	// convert banner file to map
+// 	asciiMap := buildAsciiMap(bannerLines)
+
+// 	// print ascii art
+// 	printAscii(text, asciiMap)
+// }
+
+//"fmt"
+
 func ReadBanner(file string) ([]string, error) {
+	/*
+	REads the banner files as bytes and stores them as slice of strings
+	*/
 	data, err := os.ReadFile(file)
 	if err != nil {
 		return nil, err
@@ -17,11 +32,14 @@ func ReadBanner(file string) ([]string, error) {
 }
 
 func BuildAsciiMap(lines []string) map[rune][]string {
+	/*
+	Maps ASCII Characters to their 8-line graphics
+	*/
 	asciiMap := make(map[rune][]string)
 
 	char := 32
 
-	for i := 0; i < len(lines); i += 9 {
+	for i := 0; i < len(lines); i +=9 {
 		asciiMap[rune(char)] = lines[i : i+8]
 		char++
 	}
@@ -30,7 +48,9 @@ func BuildAsciiMap(lines []string) map[rune][]string {
 }
 
 func PrintAscii(text string, asciiMap map[rune][]string) {
-
+	/*
+	Prints the characters row by row
+	*/
 	lines := strings.Split(text, "\\n")
 
 	for _, line := range lines {
@@ -41,12 +61,11 @@ func PrintAscii(text string, asciiMap map[rune][]string) {
 		}
 
 		for row := 0; row < 8; row++ {
-
 			for _, char := range line {
 				fmt.Print(asciiMap[char][row])
 			}
 
 			fmt.Println()
-		}
+		} 
 	}
 }
--- main.go
+++ main.go
@@ -3,25 +3,45 @@ package main
 import (
 	"fmt"
 	"os"
-
 	"ascii-art/ascii"
 )
 
 func main() {
-
+	/*
+	This function reads the user input from os.Args and sends it to the Render
+	function which handles the ASCII art generation.
+	*/
 	if len(os.Args) != 2 {
+		fmt.Println("Error: Invalid number of arguments")
+		fmt.Println("Usage: go run . \"text\"")
 		return
 	}
 
 	input := os.Args[1]
 
-	bannerLines, err := ascii.ReadBanner("thinkertoy.txt")
+	bannerLInes, err := ascii.ReadBanner("standard.txt")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	asciiMap := ascii.BuildAsciiMap(bannerLines)
+	asciiMap := ascii.BuildAsciiMap(bannerLInes)
 
 	ascii.PrintAscii(input, asciiMap)
 }
+
+// ----------------
+// 	// I am using this section for testing the reading of the file to the CLI
+// 	lines, err := ascii.ReadBanner("standard.txt")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+
+// 	asciiMap := ascii.BuildAsciiMap(lines)
+// 	//fmt.Println((asciiMap))
+
+// // 	for _, line := range lines {
+// // 		fmt.Println(line)
+// // 	}
+
